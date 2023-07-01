08c70207db07ecc0684f0df860d7d6ae05fd9160
Lvemiy9 <138276291+Lvemiy9@users.noreply.github.com>
2023-07-01 17:09:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -71,7 +71,27 @@ const USER_CONFIG = {
     courseSchedule: false,
   },
   
- //天行
+ //
+    TIAN_API: {
+    // 天行API KEY，如果使用天行API则需要填写此项
+    key: '5f5fb5991edc3152382c27da42380959',
+    
+    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
+    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    morningGreeting: true,
+  
+    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    eveningGreeting: true,
+  
+    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: 1,
+  
+    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    networkHot: 3,
+  
+    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
+    networkHotType: 'title',
+  },
   
   /** 是否给文字设置多彩颜色, 和emoji不兼容 */
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
