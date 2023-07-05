f606e4a793c156f008084752c5307cceda41ec07
Lvemiy9 <138276291+Lvemiy9@users.noreply.github.com>
2023-07-05 18:40:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -115,7 +115,7 @@ const USER_CONFIG = {
       id: 'oQMeP6ZJz7k49WyPoiB2RDZRVZG8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'Al3-BnbjejlxmfrpgRcPM75LYrl9WmTNbztlpHVDrgU',
+      useTemplateId: 'cYBnTiIqnMoAYmnxL189CDi5qdNYirf4fD15RvmmOXg',
       // 所在省份或城市，也可以不填
       province: '辽宁',
       // 所在城市或县区
@@ -227,7 +227,7 @@ const USER_CONFIG = {
       id: 'oQMeP6XpDQfBHFAOGtQOxygXMg0w',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'Al3-BnbjejlxmfrpgRcPM75LYrl9WmTNbztlpHVDrgU',
+      useTemplateId: 'cYBnTiIqnMoAYmnxL189CDi5qdNYirf4fD15RvmmOXg',
       // 所在省份或城市，也可以不填
       province: '辽宁',
       // 所在城市或县区
