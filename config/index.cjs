a0dd714a51b656beb411cf4d195146e04249ebf5
Lvemiy9 <138276291+Lvemiy9@users.noreply.github.com>
2023-07-12 03:04:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -112,10 +112,10 @@ const USER_CONFIG = {
       name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       // 使用其他通道时，请严格按照各个通道的教程进行填写
-      id: 'oQMeP6ZJz7k49WyPoiB2RDZRVZG8',
+      id: '、、 oQMeP6ZJz7k49WyPoiB2RDZRVZG8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'cYBnTiIqnMoAYmnxL189CDi5qdNYirf4fD15RvmmOXg',
+      useTemplateId: 'W9bQYtm2EAOP5N87MIIxyWxN1VvXI5ep46WnvszfNow',
       // 所在省份或城市，也可以不填
       province: '陕西',
       // 所在城市或县区
@@ -227,7 +227,7 @@ const USER_CONFIG = {
       id: 'oQMeP6XpDQfBHFAOGtQOxygXMg0w',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'cYBnTiIqnMoAYmnxL189CDi5qdNYirf4fD15RvmmOXg',
+      useTemplateId: 'W9bQYtm2EAOP5N87MIIxyWxN1VvXI5ep46WnvszfNow',
       // 所在省份或城市，也可以不填
       province: '陕西',
       // 所在城市或县区
